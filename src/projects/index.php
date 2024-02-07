e0eb891d00b771a5a6d35d36bf8632e63b8d2ef5
Anton Pykhtunov <recowet@gmail.com>
2024-02-07 18:08:07 +0400

    Updated projects and tasks.

--- src/projects/index.php
+++ src/projects/index.php
@@ -1,5 +1,24 @@
 <?php include '../include/header.php';?>
             
-            <h2>PROJECTS</h2>   
+<h2>PROJECTS</h2>   
+
+<table with="100%" border="1" cellpadding="10">
+    <tr>
+        <th width="40" nowrap="nowrap">№</th>    
+        <th width="200" nowrap="nowrap" align="left">NAME</th>
+        <th width="100%" align="left">DESCRIPTION</th>
+    </tr>
+
+    <tr>
+        <td align="center">01.</td>
+        <td align="left">DEMO</td>
+        <td align="left">DEMO PROJECT...</td>
+    </tr>
+    <tr>
+        <td align="center">02.</td>
+        <td align="left">TEST</td>
+        <td align="left">TEST PROJECT...</td>
+    </tr>
+</table>        
 
 <?php include '../include/footer.php';?>
--- src/tasks/index.php
+++ src/tasks/index.php
@@ -1,5 +1,24 @@
 <?php include '../include/header.php';?>
             
-            <h2>TASKS</h2>   
+ <h2>TASKS</h2>   
+
+ <table with="100%" border="1" cellpadding="10">
+    <tr>
+        <th width="40" nowrap="nowrap">№</th>    
+        <th width="200" nowrap="nowrap" align="left">NAME</th>
+        <th width="100%" align="left">DESCRIPTION</th>
+    </tr>
+
+    <tr>
+        <td align="center">01.</td>
+        <td align="left">FIRST</td>
+        <td align="left">FIRST TASK...</td>
+    </tr>
+    <tr>
+        <td align="center">02.</td>
+        <td align="left">SECOND</td>
+        <td align="left">SECOND TASK...</td>
+    </tr>
+</table>  
 
 <?php include '../include/footer.php';?>
