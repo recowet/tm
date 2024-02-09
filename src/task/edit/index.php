9a657c790c90c13fdf8c56abcc8a44266bca6a21
Anton Pykhtunov <recowet@gmail.com>
2024-02-09 18:38:21 +0400

    Added updated task.

--- src/task/edit/index.php
+++ src/task/edit/index.php
@@ -5,4 +5,50 @@
 
 <h2>EDIT TASKS</h2>
 
+<?php 
+
+    $id = $_GET['id']; 
+
+    $stmt = mysqli_prepare(
+        $connection, 
+        "SELECT name, description FROM tm.tm_task WHERE id=?; "
+    );
+
+    mysqli_stmt_bind_param($stmt, "s", $id);
+
+    $stmt->execute();
+    $stmt->store_result();
+    
+    $stmt->bind_result($name, $description);
+
+    $result = $stmt->fetch();
+
+?>
+
+<!-- <h2>EDIT TASK</h2> -->
+
+<form action="/task/save/" method="POST">
+
+        <input type="hidden" name="id" value="<?php echo $id; ?>" />
+
+    <p>
+        <div>NAME</div>
+        <div><input name="name" type="text" style="width: 300px;" value="<?php echo $name; ?>"/></div>
+    </p>
+
+    <p>
+        <div>DESCRIPTION</div>
+        <div>
+            <textarea name="description" style="width: 300px;"><?php echo $description; ?></textarea>
+        </div>
+    </p>
+
+    <p>
+        <button type="submit">
+            SAVE
+        </button>    
+    </p>
+
+ </form>
+
 <?php include '../../include/footer.php';?>
---
+++ src/task/save/index.php
@@ -0,0 +1,20 @@
+<?php
+
+include '../../config/settings.php'; 
+include '../../include/database.php'; 
+
+$id = $_POST['id'];
+$name = $_POST['name'];
+$description = $_POST['description'];
+
+$stmt = mysqli_prepare(
+    $connection, 
+    "UPDATE `tm_task` SET name = ?, description = ? WHERE id=? "
+);
+
+var_dump($stmt);
+
+mysqli_stmt_bind_param($stmt, "sss", $name, $description, $id);
+mysqli_stmt_execute($stmt);
+
+header('location: /tasks/');
